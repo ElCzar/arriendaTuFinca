2840475309a0a2b047299d17900c7d1fe1cce356
ElCzar <cesarandresolarte@gmail.com>
2024-08-25 15:54:28 -0500

    first commit

---
+++ src/test/java/com/gossip/arrienda_tu_finca/ArriendaTuFincaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gossip.arrienda_tu_finca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArriendaTuFincaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
